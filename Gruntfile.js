d51af83e76855ec3e5db85e09ffbf1ffca99a7a5
Shannon Wrege <sbwrege2z@gmail.com>
2019-07-09 14:30:27 +0000

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,17 @@
 'use strict';
 
+const path = require('path');
 const tasks = require('./lib/grunt/tasks');
 const multitask = require('./lib/grunt/shared/multitask');
 
 module.exports = function(grunt) {
-  require('load-grunt-config')(grunt);
-
-  grunt.registerMultiTask('simple', 'Log some stuff.', tasks.simple);
-
-  grunt.registerMultiTask('test', 'Test task', function() {
-    let { options } = multitask.initialize(grunt, this);
-    grunt.log.writeln('options = ' + JSON.stringify(options, null, 2));
+  require('load-grunt-config')(grunt, {
+    init: true,
+    configPath: [path.join(process.cwd(), 'grunt'), path.join(process.cwd(), 'grunt/shared')]
   });
+
+  grunt.registerMultiTask('test', 'Test task', tasks.test);
+  grunt.registerMultiTask('simple', 'Log some stuff.', tasks.simple);
 
   tasks.mapDefaults('test', 'target1');
 };
