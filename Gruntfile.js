5bdf17f2649a817371a65dfc2c79720f8ca41339
Shannon Wrege <sbwrege2z@gmail.com>
2019-07-09 15:52:37 +0000

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const path = require('path');
+const grunt_ = require('grunt');
 const tasks = require('./lib/grunt/tasks');
 const multitask = require('./lib/grunt/shared/multitask');
 
 module.exports = function(grunt) {
+  console.log(grunt === grunt_);
   require('load-grunt-config')(grunt, {
     init: true,
     configPath: [path.join(process.cwd(), 'grunt'), path.join(process.cwd(), 'grunt/shared')]
