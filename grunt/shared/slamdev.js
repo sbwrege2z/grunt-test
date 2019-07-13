b0148c2d067028d38cd64a86296df9f6ddafab17
Dallas Wrege <dallas.wrege@evolving.com>
2019-07-13 10:14:43 -0400

    .

--- grunt/shared/slamdev.js
+++ grunt/shared/slamdev.js
@@ -10,7 +10,7 @@ let slamdev = (module.exports = {
   },
   checkout: { options: { branch: null } },
   commit: { options: {} },
-  completeMerge: { options: {} },
+  completeMerge: { options: { prefix: null } },
   initializeBranch: { options: {} },
   mergeToDevelopment: {
     options: {
